6be1c7a3cd11f70a235e25f57e5feaf482c64dc9
Owen <owen.kane54@gmail.com>
2022-12-17 12:36:28 +0000

    connect to planetscale database instead of local

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,77 +1,78 @@
 const pool = require("../db");
+const connection = require("../server");
 const router = require("express").Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const response = await pool.query('SELECT * FROM posts');
+// router.get('/', async (req, res) => {
+//     try {
+//         // const response = await pool.query('SELECT * FROM posts');
 
-        const posts = response.rows;
+//         // const posts = response.rows;
 
-        res.send(posts);
+//         res.send(posts);
 
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('server error');
-    }
-}
-)
-router.get('/:username', async (req, res) => {
-    try {
-        const { username } = req.params;
-        console.log(username)
-        const response = await pool.query('SELECT * FROM posts WHERE username = $1', [username]);
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('server error');
+//     }
+// }
+// )
+// router.get('/:username', async (req, res) => {
+//     try {
+//         const { username } = req.params;
+//         console.log(username)
+//         const response = await pool.query('SELECT * FROM posts WHERE username = $1', [username]);
 
-        const posts = response.rows;
+//         const posts = response.rows;
 
-        res.send(posts);
+//         res.send(posts);
 
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('server error');
-    }
-})
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('server error');
+//     }
+// })
 
-router.post('/new', async (req, res) => {
-    try {
-        console.log(req.body);
-        const {username, data} = req.body;
-        console.log(username, data);
+// router.post('/new', async (req, res) => {
+//     try {
+//         console.log(req.body);
+//         const {username, data} = req.body;
+//         console.log(username, data);
 
-        const response = await pool.query('INSERT INTO posts (username, data) VALUES ($1, $2)', [username, data]);
+//         const response = await pool.query('INSERT INTO posts (username, data) VALUES ($1, $2)', [username, data]);
 
-        res.status(200).send('Posted');
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('server error');
-    }
-})
+//         res.status(200).send('Posted');
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('server error');
+//     }
+// })
 
-router.put('/:id', async (req, res) => {
-    try {
-        const { data } = req.body;
-        const { id } = req.params;
+// router.put('/:id', async (req, res) => {
+//     try {
+//         const { data } = req.body;
+//         const { id } = req.params;
         
-        const response = await pool.query('UPDATE posts SET data = $1 WHERE id = $2', [data, id]);
+//         const response = await pool.query('UPDATE posts SET data = $1 WHERE id = $2', [data, id]);
 
-        res.status(200).send('Post edited');
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('server error');
-    }
-})
+//         res.status(200).send('Post edited');
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('server error');
+//     }
+// })
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        console.log(id);
+// router.delete('/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         console.log(id);
 
-        const response = await pool.query('DELETE FROM posts WHERE id = $1', [id]);
+//         const response = await pool.query('DELETE FROM posts WHERE id = $1', [id]);
 
-        res.status(200).send('Post deleted');
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('server error');
-    }
-})
+//         res.status(200).send('Post deleted');
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('server error');
+//     }
+// })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("./utils/jwtGenerator");
+const authorization = require("./middleware/authorization");
 
 const app = express();
 
@@ -8,8 +11,133 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
+require('dotenv').config()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM posts', (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    });
+})
+app.get('/posts', (req, res) => {
+    connection.query('SELECT * FROM posts', (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    })
+})
+app.get('/posts/:username', (req, res) => {
+    const { username } = req.params;
+    console.log(username);
+    connection.query(`SELECT * FROM posts WHERE username = "${username}"`, (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    })
+})
+app.post('/posts/new', (req, res) => {
+    const { username, data } = req.body;
+    console.log(username, data);
+    connection.query(`INSERT INTO posts (username, data) VALUES ("${username}", "${data}")`, (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    })
+})
+app.put('/posts/:id', (req, res) => {
+    const { data } = req.body;
+    const { id } = req.params;
+    connection.query(`UPDATE posts SET data = "${data}" WHERE id = "${id}"`, (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    })
+})
+app.delete('/posts/:id', (req, res) => {
+    const { id } = req.params;
+    connection.query(`DELETE FROM posts WHERE id = "${id}"`, (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    })
+})
+
+
+
+app.post('/user/register', async (req, res) => {
+    try {
+        const {username, email, password } = req.body;
+
+        // check if user already exists with email
+        const isEmail = connection.query(`SELECT * FROM users WHERE email = "${email}"`, (err, rows, fields) => {
+            if (err) throw err;
+            console.log(rows);
+            if (rows.length !== 0) {
+                return res.send('Email already registered. Log in now')
+            }
+        });
+        // check if username already in use
+        const isUser = connection.query(`SELECT * FROM users WHERE username = "${username}"`, (err, rows, fields) => {
+            if (err) throw err;
+            console.log(rows);
+            if (rows.length !== 0) {
+                return res.send('Username already in use, pick another')
+            }
+        });
+
+        // encrypt password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // add user to database
+        connection.query(`INSERT INTO users (username, email, password) VALUES ("${username}", "${email}", "${hashedPassword}")`, (err, rows, fields) => {
+            if (err) throw err; 
+            res.send('User registered')
+        })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+})
+
+app.post('/user/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // check if user doesn't exist
+    const user = connection.query(`SELECT * FROM users WHERE username = "${username}"`, async (err, rows, fields) => {
+        if (err) throw err;
+        if (rows.length === 0) {
+            return res.status(401).send('Username or password is incorrect');
+        }
+        console.log(rows[0].password);
+        // check if input password matches database password
+
+        const validPassword = await bcrypt.compare(password, rows[0].password);
+
+        if (!validPassword) {
+            return res.status(401).send('Username or password is incorrect');
+        }
+
+        const token = jwtGenerator(rows[0].username);
+
+        res.json({ token });
+    });
+})
+
+app.get('/user/is-verify', authorization, async (req, res) => {
+    try {
+        res.json(true);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+})
+
+
+
+// app.use('/posts', require("./routes/posts"));
+
+// connection.end()
+
+
 app.listen(5000, () => console.log('server listening on port 5000'));
 
-app.use('/posts', require("./routes/posts"));
 
-app.use('/user', require("./routes/users"));
+// app.use('/user', require("./routes/users"));
